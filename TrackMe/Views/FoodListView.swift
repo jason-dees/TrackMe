b943c45dd1e6d21666a6020ee404a7abe6c611af
Jason Dees <jhdees@gmail.com>
2022-12-25 18:57:45 -0500

    feat: food list navigation and search and jumping to items

--- TrackMe/Views/FoodListView.swift
+++ TrackMe/Views/FoodListView.swift
@@ -7,26 +7,82 @@
 
 import SwiftUI
 
+//https://stackoverflow.com/questions/58809357/swiftui-list-with-section-index-on-right-hand-side
 struct FoodListView: View {
     @StateObject var viewModel: FoodListViewModel
     @State var searchText  = ""
+    @State var currentLetter = ""
     
     var body: some View {
-        NavigationView{
-            List {
-                ForEach(viewModel.foods, id: \.id) { food in
-                    HStack {
-                        Image(systemName: food.icon)
-                        Text(food.name)
+        ScrollViewReader { scrollProxy in
+            ZStack {
+                NavigationView{
+                    List {
+                        ForEach(filteredAlphabet, id: \.self) { letter in
+                            Section(header: Text(letter).id(letter)) {
+                                ForEach(searchResults.filter { $0.name.prefix(1) == letter }, id: \.id) { food in
+                                    NavigationLink(destination: Text(food.name)){
+                                        Text(food.name)
+                                    }
+                                }
+                            }
+                        }
+                        
+                    }
+                    .listStyle(.grouped)
+                    .navigationTitle("Foods")
+                    .searchable(text: $searchText,
+                                placement: .navigationBarDrawer(displayMode: .always),
+                                prompt: "Search foods")
+                }
+                VStack {
+                    ForEach(alphabet, id: \.self) { letter in
+                        HStack {
+                            Spacer()
+                            if filteredAlphabet.contains { $0 == letter }{
+                                Button(action: {
+                                    print("letter = \(letter)")
+                                    scrollProxy.scrollTo(letter)
+                                }, label: {
+                                    Text(letter)
+                                        .font(.system(size: 12))
+                                        .padding(.trailing, 7)
+                                })
+                            }
+                            else {
+                                Button(action: {
+                                }, label: {
+                                    Text(letter)
+                                        .font(.system(size: 12))
+                                        .padding(.trailing, 7)
+                                }).disabled(true)
+                            }
+                        }
                     }
                 }
             }
-            .listStyle(.grouped)
-            .navigationTitle("Foods")
         }
-        .searchable(text: $searchText,
-                    placement: .navigationBarDrawer(displayMode: .always),
-                    prompt: "Search foods")
+    }
+    var searchResults: [Food] {
+        if searchText.isEmpty {
+            return viewModel.foods
+        } else {
+            return viewModel.foods.filter { $0.name.lowercased().contains(searchText.lowercased()) }
+        }
+    }
+    
+    let alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+    
+    var filteredAlphabet: [String] {
+        alphabet.filter { a in searchResults.contains { f in a == f.name.prefix(1)} }
+    }
+    
+    func isSameLetter(_ foodName: String) -> Bool {
+        if currentLetter != foodName.prefix(1) {
+            currentLetter = String(foodName.prefix(1))
+            return false
+        }
+        return true
     }
 }
 
---
+++ TrackMe/Views/FoodView.swift
@@ -0,0 +1,20 @@
+//
+//  FoodView.swift
+//  TrackMe
+//
+//  Created by Jason Dees on 12/25/22.
+//
+
+import SwiftUI
+
+struct FoodView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct FoodView_Previews: PreviewProvider {
+    static var previews: some View {
+        FoodView()
+    }
+}
